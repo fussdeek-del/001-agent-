8e27bff77fc90e458527f50fba05241af513ddd4
Nabeel Ahmed <fussdeek@gmail.com>
2026-06-15 14:02:47 +0500

    yoyoyooyoy

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import pkg from '@slack/bolt';
 const { App } = pkg;
 import { Webchain } from '@slack/web-api';
@@ -11,5 +13,23 @@ dotenv.config();
 const log = {
     info: (msg ...args) => console.log('[INFO]', ${msg}', ...args),
     error: (msg ...args) => console.log('[ERROR]', ${msg}', ...args),
-    debug: (msg ...args) => console.log('[DEBUG]', ${msg}', ...args)
+    debug: (msg ...args) => process.env.NODE_ENV === 'development' && console.log('[DEBUG]', ${msg}', ...args)),'
+
+}
+
+
+class SlackAIAgen {
+    constructor() {
+        this.app = expess()
+        this.slack = new app ({
+            token: process.env.SLACK_BOT_TOKEN,
+            signingSecret: process.env.SLACK_SIGNING_SECRET,
+            scoketMode: true,
+            appToken: process.env.SLACK_APP_TOKEN,
+        });
+        this.webClint = new this.webClint(process.env.SLACK_BOT_TOKEN);
+        this.openai = new ChatOpenAI({
+            model: "gpt-4",
+            apiKey: process.env.OPENAI_API_KEY,
+    }
 }
